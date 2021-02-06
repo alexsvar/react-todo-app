543dd119f43895a40c78ba18369774817c260bd9
Alexander Morozov <alexsvar@icloud.com>
2021-02-06 15:23:20 -0800

    Added the function of deleting an element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
@@ -7,25 +7,48 @@ import ItemStatusFilter from '../item-status-filter'
 
 import './app.css'
 
-const App = () => {
 
-  const todoData = [
-    {label: 'Drink Coffee', important: false, id: 1},
-    {label: 'Make Awesome App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3}
-  ]
+export default class App extends Component {
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel/>
-        <ItemStatusFilter/>
+  state = {
+    todoData: [
+      {label: 'Drink Coffee', important: false, id: 1},
+      {label: 'Make Awesome App', important: true, id: 2},
+      {label: 'Have a lunch', important: false, id: 3}
+    ]
+  }
+
+  deleteItem = id => {
+    this.setState(({todoData}) => {
+      const index = todoData.findIndex(el => el.id === id)
+      const newArray = [
+        ...todoData.slice(0, index),
+        ...todoData.slice(index + 1)
+      ]
+
+      return {
+        todoData: newArray
+      }
+    })
+  }
+
+  render() {
+    const {todoData} = this.state
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel/>
+          <ItemStatusFilter/>
+        </div>
+
+        <TodoList
+          todos={todoData}
+          onDeleted={this.deleteItem}
+        />
       </div>
-
-      <TodoList todos={todoData}/>
-    </div>
-  )
+    )
+  }
 }
 
-export default App

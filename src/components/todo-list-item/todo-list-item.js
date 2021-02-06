8b7fd798d26697502675a7d21d32a6c03786bf41
Alexander Morozov <alexsvar@icloud.com>
2021-02-06 13:52:09 -0800

    Added events

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,6 +4,10 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`)
+  }
+
   render() {
     const {label, important = false} = this.props
 
@@ -14,22 +18,23 @@ export default class TodoListItem extends Component {
 
     return (
       <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation"/>
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o"/>
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation"/>
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o"/>
-      </button>
-    </span>
     )
   }
 

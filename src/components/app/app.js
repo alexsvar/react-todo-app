f13373cb477b0af9e0b9e430045aea441b4747be
Alexander Morozov <alexsvar@icloud.com>
2021-02-08 15:58:08 -0800

    Added search panel functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have A Lunch')
-    ]
+    ],
+    term: ''
   }
 
   createTodoItem(label) {
@@ -89,8 +90,22 @@ export default class App extends Component {
     })
   }
 
+  onSearchChange = term => {
+    this.setState({term})
+  }
+
+  search(items, term) {
+    if (term.length === 0) {
+      return items
+    }
+
+    return items.filter(item => item.label.toLowerCase()
+      .indexOf(term.toLowerCase()) > -1)
+  }
+
   render() {
-    const {todoData} = this.state
+    const {todoData, term} = this.state
+    const visibleItems = this.search(todoData, term)
     const doneCount = todoData
       .filter(el => el.done).length
     const todoCount = todoData.length - doneCount
@@ -99,12 +114,13 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount}/>
         <div className="top-panel d-flex">
-          <SearchPanel/>
+          <SearchPanel
+            onSearchChange={this.onSearchChange}
+          />
           <ItemStatusFilter/>
         </div>
-
         <TodoList
-          todos={todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
